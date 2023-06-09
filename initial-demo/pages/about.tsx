317ad031f6b4ab228a2862f58d354761ce16b71a
rexdaro <rexdaro@gmail.com>
2023-06-09 19:46:26 -0300

    ir a about - ir a home primera clase

--- initial-demo/pages/about.tsx
+++ initial-demo/pages/about.tsx
@@ -4,11 +4,11 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function HomePage() {
+export default function aboutPage() {
   return (
     <>
       <Head>
-        <title>Home - Manuel</title>
+        <title>About - Manuel</title>
         <meta name="description" content="Home Page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -16,7 +16,7 @@ export default function HomePage() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           
-        <h1>Manuel Rivas H1</h1>
+        <h1>Esta pagina es el about</h1>
 
         </div>
 
@@ -24,12 +24,12 @@ export default function HomePage() {
 
         <div className={styles.grid}>
           <a
-            href="/about"
+            href="/"
             className={styles.card}            
             rel="noopener noreferrer"
           >
             <h2>
-              ir al about <span>-&gt;</span>
+              ir a Home <span>-&gt;</span>
             </h2>
             <p>
               Find in-depth information about Next.js features and&nbsp;API.
